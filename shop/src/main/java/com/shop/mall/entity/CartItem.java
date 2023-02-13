3d73f11e17cceca0f607706a51e30a618eb33017
hyerilim <lhr317@naver.com>
2023-02-13 09:08:16 +0900

    entity 확인

--- shop/src/main/java/com/shop/mall/entity/CartItem.java
+++ shop/src/main/java/com/shop/mall/entity/CartItem.java
@@ -2,6 +2,7 @@ package com.shop.mall.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -11,24 +12,39 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
 @Table(name="cart_item")
-public class CartItem extends BaseEntity{
-	
+public class CartItem extends BaseEntity {
+
     @Id
     @GeneratedValue
-    @Column(name="cart_item_id")
+    @Column(name = "cart_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="cart_id")
     private Cart cart;
 
-    @ManyToOne
-    @JoinColumn(name="item_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
     private Item item;
 
     private int count;
+
+    public static CartItem createCartItem(Cart cart, Item item, int count) {
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setItem(item);
+        cartItem.setCount(count);
+        return cartItem;
+    }
+
+    public void addCount(int count){
+        this.count += count;
+    }
+
+    public void updateCount(int count){
+        this.count = count;
+    }
 
 }
--- shop/src/main/java/com/shop/mall/entity/Order.java
+++ shop/src/main/java/com/shop/mall/entity/Order.java
@@ -68,4 +68,6 @@ public class Order extends BaseEntity {
         }
         return totalPrice;
     }
+
+
 }
