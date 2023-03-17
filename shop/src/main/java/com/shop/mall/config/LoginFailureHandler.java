9d5fb55d61ef7620f2a2faa4e8578e1729a43434
taejong12 <dbxowhdsla12@naver.com>
2023-03-17 16:14:14 +0900

    test3

--- shop/src/main/java/com/shop/mall/config/LoginFailureHandler.java
+++ shop/src/main/java/com/shop/mall/config/LoginFailureHandler.java
@@ -1,25 +1,25 @@
-package com.shop.mall.config;
-
-import java.io.IOException;
-
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.stereotype.Component;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Component
-public class LoginFailureHandler implements AuthenticationFailureHandler{@Override
-	
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException exception) throws IOException, ServletException {
-		log.info("로그인 실패");
-		response.sendRedirect("/members/login");
-		
-	}
-
-}
+//package com.shop.mall.config;
+//
+//import java.io.IOException;
+//
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+//import org.springframework.stereotype.Component;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//@Component
+//public class LoginFailureHandler implements AuthenticationFailureHandler{@Override
+//	
+//	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+//			AuthenticationException exception) throws IOException, ServletException {
+//		log.info("로그인 실패");
+//		response.sendRedirect("/members/login");
+//		
+//	}
+//
+//}
